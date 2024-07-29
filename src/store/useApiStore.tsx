d7ce784382b2f559115568a3a51d98fa2941b824
Andrey Uvikov <andrey.uvikov@axenix.pro>
2024-07-29 11:08:48 +0300

    add interceptor

--- src/store/useApiStore.tsx
+++ src/store/useApiStore.tsx
@@ -4,6 +4,7 @@ import { api } from "../api/axios";
 export const useApiStore = create((set, get) => ({
   postsData: [],
   fetchPosts: async () => {
+    api.interceptors.response.use((response) => response);
     const data = await api.get().then((response) => response.data);
     set((state) => {
       return { ...state, postsData: [...data] };
